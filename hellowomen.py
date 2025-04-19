a8b7c7a40d9f659487b39a6d63263170e4390ea6
Gabriela Bado <gabibado92@gmail.com>
2025-04-19 15:27:34 -0300

    Add arquivo callHelloWomen.py via upload
    Adicionando um arquivo em Python com a função callHelloWomen para imprimir uma mensagem personalizada sobre o curso GitHub4Women, com entrada do nome do curso pelo usuário

---
+++ hellowomen.py
@@ -0,0 +1,7 @@
+# Este é um script simples de Python para imprimir uma mensagem
+
+def callHelloWomen():
+    curso = input("Digite o nome do curso: ")
+    print(f"Hello {curso}! Estou muito feliz de entender mais os conceitos de GitHub nessas aulas!")
+
+callHelloWomen()
