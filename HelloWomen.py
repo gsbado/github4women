938520f5bcec1b37dbd4ec4f8abd17485d265f2f
gsbado <gabibado92@gmail.com>
2025-04-19 16:03:14 -0300

    Novo arquivo teste de Python

---
+++ HelloWomen.py
@@ -0,0 +1,8 @@
+# Este é um script simples de Python para imprimir uma mensagem
+
+def callHelloWomen():
+    nome = input("Digite seu nome: ")
+    curso = input("Digite o nome do curso: ")
+    print(f"Hello {curso}! Eu, {nome}, estou muito feliz de entender mais os conceitos de GitHub nessas aulas, pois isso me ajudará muito na minha carreira profissional.")
+
+callHelloWomen()
